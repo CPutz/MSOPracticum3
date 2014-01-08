337d82f382be5d771e5b423365997306bd72396b
Casper_Laptop <css_mn@yahoo.ie>
2014-01-08 14:49:45 +0100

    partially did part 4 (adding FormGraphics class)

---
+++ ShapeDrawing/ShapeDrawing/ShapeDrawing/FormGraphics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ShapeDrawing
+{
+    class FormGraphics : IGraphics
+    {
+        private Graphics canvas;
+
+        public FormGraphics(Graphics canvas)
+        {
+            this.canvas = canvas;
+        }
+
+        public void DrawPolyLine(Point[] points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawCircle(int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
