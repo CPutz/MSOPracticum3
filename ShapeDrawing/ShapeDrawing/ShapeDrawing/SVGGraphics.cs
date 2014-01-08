7072e03e39ff67704adc1fb5298deacb62689d3c
Casper_Desktop <css_mn@yahoo.ie>
2014-01-08 18:23:45 +0100

    start with SVGGraphics class

---
+++ ShapeDrawing/ShapeDrawing/ShapeDrawing/SVGGraphics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+
+class SVGGraphics : IGraphics
+{
+    private string filename;
+
+
+    public SVGGraphics(string filename)
+    {
+        this.filename = filename;
+
+        FileStream stream = new FileStream(this.filename, FileMode.Create);
+        StreamWriter writer = new StreamWriter(stream);
+
+        writer.WriteLine("<?xml version=\"1.0\" standalone=\"no\"?>");
+        writer.WriteLine("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"");
+        writer.WriteLine("\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">");
+        writer.WriteLine("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">");
+
+        writer.Close();
+        stream.Close();
+    }
+
+
+    public void DrawPolyLine(Point[] points)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DrawCircle(int x, int y, int size)
+    {
+        throw new NotImplementedException();
+    }
+}
