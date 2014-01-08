fd3b1c0876883493769fa9e5147a69b796aa626c
Casper_Laptop <css_mn@yahoo.ie>
2014-01-08 14:37:25 +0100

    added Point struct

---
+++ ShapeDrawing/ShapeDrawing/ShapeDrawing/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShapeDrawing
+{
+    struct Point
+    {
+        private readonly int x;
+        private readonly int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+    }
+}
